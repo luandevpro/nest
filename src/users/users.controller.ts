0b31e005e5bd378ca0b5777be11e7b8f416d0441
luandevpro <luandevpro@gmail.com>
2020-09-29 13:34:30 +0700

    crud api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get, Redirect, Param } from '@nestjs/common';
+import { Controller, Get, Delete, Param, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+
+import { User } from '../schemas/users.schema';
+
+import { CreateUserDto } from '../dto/user.dto';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('users')
-  getAll(): string {
-    return this.usersService.getAll();
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
+  @Get('users/:id')
+  getUser(@Param('id') id: string): Promise<User> {
+    return this.usersService.getUser(id);
+  }
+
+  @Post('users')
+  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Post('users/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: CreateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Delete('users/:id')
+  deleteUser(@Param('id') id: string): Promise<User> {
+    return this.usersService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { User, UserDocument } from '../schemas/users.schema';
+
+import { CreateUserDto } from '../dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getAll(): string {
-    return 'All User';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async getUsers(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async getUser(id: String): Promise<User> {
+    return this.userModel.findOne({ _id: id }).exec();
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return this.userModel.create(createUserDto);
+  }
+
+  async updateUser(id: String, createUserDto: CreateUserDto): Promise<User> {
+    return this.userModel.findOneAndUpdate({ _id: id }, createUserDto, {
+      new: true,
+      upsert: false,
+      runValidators: true,
+    });
+  }
+
+  async deleteUser(id: String): Promise<User> {
+    return this.userModel.findOneAndRemove({ _id: id });
   }
 }
