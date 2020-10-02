fb591db86be3eaf660991354c0dd42cab1d8cf85
luandevpro <luandevpro@gmail.com>
2020-10-02 08:34:03 +0700

    logger provider dynamics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { UsersModule } from './users/users.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [LoggerModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
